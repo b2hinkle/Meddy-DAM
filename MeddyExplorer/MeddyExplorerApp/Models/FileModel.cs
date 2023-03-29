4c1c25701df87366bfe1be1f1f671af49f3e7157
Brian2524 <b2hinkle@gmail.com>
2023-03-29 08:57:00 -0400

    Start off explorer functionality by making the root directory's files shown when you make the Meddy project

---
+++ MeddyExplorer/MeddyExplorerApp/Models/FileModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MeddyExplorerApp.Models
+{
+    internal class FileModelFrontEnd
+    {
+        public FileModelFrontEnd(string inName, string inDateModified, string inType, string inSize) 
+        { 
+            Name = inName;
+            DateModified = inDateModified;
+            Type = inType;
+            Size = inSize;
+        }
+        public string Name { get; set; }
+        public string DateModified { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+    }
+}
