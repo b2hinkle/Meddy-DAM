5d3f0d69c07dfa6ac3acc9aaf16c0c2d869ef5e1
Brian2524 <b2hinkle@gmail.com>
2023-03-29 02:24:50 -0400

    Implement folder picker from the MAUI Community Toolkit for creating a new meddy project on startup screen. Still need to see if it works on all devices (crashes on android emulator right now for some reason).

--- MeddyExplorer/MeddyExplorerApp/MauiProgram.cs
+++ MeddyExplorer/MeddyExplorerApp/MauiProgram.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
 using MudBlazorExtensionLibrary.Services;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Storage;
 
 namespace MeddyExplorerApp;
 
@@ -11,12 +13,14 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .UseMauiApp<App>().UseMauiCommunityToolkitCore()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
 
-		builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddSingleton<IFolderPicker>(FolderPicker.Default);
+        builder.Services.AddMauiBlazorWebView();
         builder.Services.AddMudServices();
 		builder.Services.AddScoped<MBELLayoutService>();
 
