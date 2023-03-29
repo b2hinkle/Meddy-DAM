ddd7ebbbf8ec25e610cd1f15fb7899bb18ffb13c
Brian2524 <b2hinkle@gmail.com>
2023-03-29 12:04:27 -0400

    Make an "up one level" button and implement its functionality. In the process a multicast delegate was created "OnCurrentDirChanged" which is a member of the MeddyExplorerSessionInfo singleton for the whole app to use.

--- MeddyExplorer/MeddyExplorerApp/Objects/MeddyExplorerSessionInfo.cs
+++ MeddyExplorer/MeddyExplorerApp/Objects/MeddyExplorerSessionInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,28 @@ namespace MeddyExplorerApp.Objects
 {
     internal class MeddyExplorerSessionInfo
     {
+        public delegate void StringDelegate(string string1, string string2);
+        public event StringDelegate OnCurrentDirChanged;
+
+
         public string RootDir { get; set; }
-        public string CurrentDir { get; set; }
+        private string currentDir;
+        public string CurrentDir
+        {
+            get { return currentDir; }
+            set 
+            {
+                string oldCurrentDir = currentDir;
+
+                currentDir = value;
+
+                if (OnCurrentDirChanged is not null)
+                {
+                    OnCurrentDirChanged.Invoke(oldCurrentDir, currentDir);
+                }
+            }
+        }
+
 
         public MeddyExplorerSessionInfo()
         {
