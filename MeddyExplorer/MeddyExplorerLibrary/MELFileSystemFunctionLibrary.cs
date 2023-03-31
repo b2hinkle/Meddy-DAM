b378f33972ac281a3b5f463a7e043db00c907055
Christian Hinkle <c4hinkle@gmail.com>
2023-03-31 06:19:51 -0400

    Make MeddyExplorerLibrary with general functions.
    Have MeddyExplorer depend on it and use it for, now separated, file system functionality.

---
+++ MeddyExplorer/MeddyExplorerLibrary/MELFileSystemFunctionLibrary.cs
@@ -0,0 +1,43 @@
+﻿namespace MeddyExplorerLibrary
+{
+    /// <summary>
+    /// Collection of helpful functions for using the file system
+    /// </summary>
+    public static class MELFileSystemFunctionLibrary
+    {
+        public static string GetFileSystemInfoSizeString(FileSystemInfo inFileSystemInfo)
+        {
+            FileInfo? fileInfo = inFileSystemInfo as FileInfo;
+            if (fileInfo is not null)
+            {
+                return fileInfo.Length.ToString();
+            }
+
+            DirectoryInfo? directoryInfo = inFileSystemInfo as DirectoryInfo;
+            if (directoryInfo is not null)
+            {
+                // Calculation for size of this directory
+                string sizeInBytes = "";
+                return sizeInBytes;
+            }
+
+            return "";
+        }
+
+        public static string GetFileSystemInfoTypeString(FileSystemInfo inFileSystemInfo)
+        {
+            FileInfo? fileInfo = inFileSystemInfo as FileInfo;
+            if (fileInfo is not null)
+            {
+                return fileInfo.Extension;
+            }
+
+            if (inFileSystemInfo is DirectoryInfo)
+            {
+                return "Folder";
+            }
+
+            return "";
+        }
+    }
+}
