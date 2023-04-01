50065e0d5f6b5ff0afdf963385f19b4698c6a7e1
Brian2524 <b2hinkle@gmail.com>
2023-04-01 11:55:10 -0400

    Create and implement recent Meddy projects list on startup page.
    - Static helper functions for saving an object to an XML file and for loading an XML file into an object.
    - Persistent data struct which gets loaded/saved on app
      startup/shutdown to keep data across sessions.

--- MeddyExplorer/MeddyExplorerApp/Objects/MeddyExplorerSessionInfo.cs
+++ MeddyExplorer/MeddyExplorerApp/Objects/MeddyExplorerSessionInfo.cs
@@ -1,24 +1,31 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MeddyExplorerApp.Objects
+﻿namespace MeddyExplorerApp.Objects
 {
-    internal class MeddyExplorerSessionInfo
+    public class MeddyExplorerSessionInfo
     {
         public delegate void DirectoryParametersDelegate(DirectoryInfo in1, DirectoryInfo in2);
+        public event DirectoryParametersDelegate OnRootDirChanged;
         public event DirectoryParametersDelegate OnCurrentDirChanged;
 
+        private DirectoryInfo rootDir;
+        public DirectoryInfo RootDir
+        {
+            get { return rootDir; }
+            set
+            {
+                DirectoryInfo oldRootDir = rootDir;
+                rootDir = value;
 
-        public DirectoryInfo RootDir { get; set; }
+                if (OnRootDirChanged is not null)
+                {
+                    OnRootDirChanged.Invoke(oldRootDir, rootDir);
+                }
+            }
+        }
         private DirectoryInfo currentDir;
         public DirectoryInfo CurrentDir
         {
             get { return currentDir; }
-            set 
+            set
             {
                 DirectoryInfo oldCurrentDir = currentDir;
 
@@ -29,6 +36,11 @@ namespace MeddyExplorerApp.Objects
                     OnCurrentDirChanged.Invoke(oldCurrentDir, currentDir);
                 }
             }
+        }
+
+        public MeddyExplorerSessionInfo()
+        {
+            OnRootDirChanged += (inOldRootDir, inNewRootDir) => App.persistentData.AddToRecentMeddyProjects(inNewRootDir);
         }
     }
 }
