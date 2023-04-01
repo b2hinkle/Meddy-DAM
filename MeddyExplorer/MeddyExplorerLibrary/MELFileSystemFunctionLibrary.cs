dc796269f6243f099f1dd98d1f1554934fd1eb09
Christian Hinkle <c4hinkle@gmail.com>
2023-04-01 06:43:11 -0400

    Separate files table functionality and update MudBlazor.
    Separate more functionality to the MeddyExplorerLibrary.

--- MeddyExplorer/MeddyExplorerLibrary/MELFileSystemFunctionLibrary.cs
+++ MeddyExplorer/MeddyExplorerLibrary/MELFileSystemFunctionLibrary.cs
@@ -1,10 +1,54 @@
-﻿namespace MeddyExplorerLibrary
+﻿using MudBlazor;
+using System;
+using static MudBlazor.CategoryTypes;
+
+namespace MeddyExplorerLibrary
 {
     /// <summary>
     /// Collection of helpful functions for using the file system
     /// </summary>
     public static class MELFileSystemFunctionLibrary
     {
+        public static void PopulateFileSystemInfoList(List<FileSystemInfo> inFileSystemInfos, string inPath)
+        {
+            string[] subfolderPaths = Directory.GetDirectories(inPath);
+            foreach (string subfolderPath in subfolderPaths)
+            {
+                inFileSystemInfos.Add(new DirectoryInfo(subfolderPath));
+            }
+
+            string[] filePaths = Directory.GetFiles(inPath);
+            foreach (string filePath in filePaths)
+            {
+                inFileSystemInfos.Add(new FileInfo(filePath));
+            }
+        }
+
+        public static string GetFileSystemInfoIcon(FileSystemInfo inFileSystemInfo)
+        {
+            if (inFileSystemInfo is FileInfo)
+            {
+                return Icons.Material.Filled.InsertDriveFile;
+            }
+
+            if (inFileSystemInfo is DirectoryInfo)
+            {
+                return Icons.Material.Filled.Folder;
+            }
+
+            return string.Empty;
+        }
+
+        public static string GetFileSystemInfoNameString(FileSystemInfo inFileSystemInfo)
+        {
+            return inFileSystemInfo.Name;
+        }
+
+        public static string GetFileSystemInfoDateString(FileSystemInfo inFileSystemInfo)
+        {
+            return inFileSystemInfo.LastWriteTime.ToString("yyyy-MM-dd hh-mm-ss");
+        }
+
         public static string GetFileSystemInfoSizeString(FileSystemInfo inFileSystemInfo)
         {
             FileInfo? fileInfo = inFileSystemInfo as FileInfo;
