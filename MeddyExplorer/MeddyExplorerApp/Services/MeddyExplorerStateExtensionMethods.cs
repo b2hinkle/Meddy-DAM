e47569c3f6d527ae6b2f0485e2a334e4085037f9
Christian Hinkle <c4hinkle@gmail.com>
2023-04-05 11:56:31 -0400

    Implement range selecting and improve selecting code.
    Settle on a temporary solution for range selecting and input.

--- MeddyExplorer/MeddyExplorerApp/Services/MeddyExplorerStateExtensionMethods.cs
+++ MeddyExplorer/MeddyExplorerApp/Services/MeddyExplorerStateExtensionMethods.cs
@@ -5,17 +5,29 @@
     /// </summary>
     public static class MeddyExplorerStateExtensionMethods
     {
+        /// <summary>
+        /// "Pointer-select"
+        /// </summary>
         public static void ExclusiveSelectFile(this MeddyExplorerState meddyExplorerState, FileSystemInfo inFile)
         {
             meddyExplorerState.ClearSelectedFiles();
-            meddyExplorerState.AddToSelectedFiles(inFile);
+            AdditiveSelectFile(meddyExplorerState, inFile);
         }
 
+        /// <summary>
+        /// "Space-select"
+        /// </summary>
         public static void AdditiveSelectFile(this MeddyExplorerState meddyExplorerState, FileSystemInfo inFile)
         {
-            meddyExplorerState.AddToSelectedFiles(inFile);
+            if (meddyExplorerState.GetSelectedFiles().Contains(inFile) == false)
+            {
+                meddyExplorerState.AddToSelectedFiles(inFile);
+            }
         }
 
+        /// <summary>
+        /// "Ctrl-select"
+        /// </summary>
         public static void AdditiveToggleSelectFile(this MeddyExplorerState meddyExplorerState, FileSystemInfo inFile)
         {
             if (meddyExplorerState.GetSelectedFiles().Contains(inFile) == false)
@@ -25,6 +37,63 @@
             else
             {
                 meddyExplorerState.RemoveFromSelectedFiles(inFile);
+            }
+        }
+
+        /// <summary>
+        /// "Shift-select"
+        /// </summary>
+        public static void ExclusiveRangeSelectFile(this MeddyExplorerState meddyExplorerState, FileSystemInfo inFile)
+        {
+            if (meddyExplorerState.GetSelectedFiles().Any() == false)
+            {
+                // Our from index is currently dependent on selected files
+                AdditiveSelectFile(meddyExplorerState, inFile); // just make sure that the file is selected
+                return;
+            }
+
+            int fromIndex = meddyExplorerState.Files.IndexOf(meddyExplorerState.GetSelectedFiles().Last()); // TODO: use a more appropriate starting point
+            int toIndex = meddyExplorerState.Files.IndexOf(inFile);
+
+            meddyExplorerState.ClearSelectedFiles();
+            AdditiveRangeSelectByIndexes(meddyExplorerState, fromIndex, toIndex);
+        }
+
+        /// <summary>
+        /// "Ctrl-Shift-select"
+        /// </summary>
+        public static void AdditiveRangeSelectFile(this MeddyExplorerState meddyExplorerState, FileSystemInfo inFile)
+        {
+            if (meddyExplorerState.GetSelectedFiles().Any() == false)
+            {
+                // Our from index is currently dependent on selected files
+                AdditiveSelectFile(meddyExplorerState, inFile); // just make sure that the file is selected
+                return;
+            }
+
+            int fromIndex = meddyExplorerState.Files.IndexOf(meddyExplorerState.GetSelectedFiles().Last()); // TODO: use a more appropriate starting point
+            int toIndex = meddyExplorerState.Files.IndexOf(inFile);
+
+            AdditiveRangeSelectByIndexes(meddyExplorerState, fromIndex, toIndex);
+        }
+
+        /// <summary>
+        /// Shared helper function.
+        /// NOTE: Indexes are relative to the MeddyExplorerState.Files list.
+        /// </summary>
+        private static void AdditiveRangeSelectByIndexes(MeddyExplorerState meddyExplorerState, int inFromIndex, int inToIndex)
+        {
+            if (inFromIndex == inToIndex)
+            {
+                // Nothing to do
+                AdditiveSelectFile(meddyExplorerState, meddyExplorerState.Files[inFromIndex]); // just make sure that the file is selected
+                return;
+            }
+
+            int iterateDirection = Math.Sign(inToIndex - inFromIndex);
+            for (int i = inFromIndex; (iterateDirection > 0) ? (i <= inToIndex) : (i >= inToIndex); i += iterateDirection)
+            {
+                AdditiveSelectFile(meddyExplorerState, meddyExplorerState.Files[i]);
             }
         }
 
